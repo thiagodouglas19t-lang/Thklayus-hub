33087d627b8436d16d6921c0b938acb036ed8c80
thiagodouglas19t-lang <thiagodouglas19t@gmail.com>
2026-04-25 14:46:07 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
+const root = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
