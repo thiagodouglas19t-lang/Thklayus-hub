8a34f9765bc2620931bb9d6907cb5da94436cc21
thiagodouglas19t-lang <thiagodouglas19t@gmail.com>
2026-04-27 15:12:19 -0300

    Cria index Canva basico

---
+++ src/data/chapters/canvaBasico/index.ts
@@ -0,0 +1,7 @@
+import { CourseChapterPack } from "../../courseChapterTypes";
+import { canvaBasicoChapter1 } from "./chapter1";
+
+export const canvaBasicoChapters: CourseChapterPack = {
+  courseId: "design-canva-profissional",
+  chapters: [canvaBasicoChapter1]
+};
