198ec42826d1d181943735d601e47fc96933d14d
thiagodouglas19t-lang <thiagodouglas19t@gmail.com>
2026-05-04 00:14:26 -0300

    Add ClashRoom types

---
+++ src/clash/types.ts
@@ -0,0 +1,24 @@
+export type Screen = "home" | "create" | "room" | "history";
+export type Mode = "battle" | "ranking";
+
+export type Option = {
+  id: number;
+  text: string;
+  votes: number;
+};
+
+export type Room = {
+  id: number;
+  title: string;
+  category: string;
+  mode: Mode;
+  options: Option[];
+  finished: boolean;
+};
+
+export type WinnerRecord = {
+  id: number;
+  roomTitle: string;
+  winner: string;
+  votes: number;
+};
